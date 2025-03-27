75553c2c12826906c5c2139d999c4806c9fe5560
Bonnette <bonnetteumu@gmail.com>
2025-03-27 07:03:48 +0300

    created java file

---
+++ index.js
@@ -0,0 +1,37 @@
+let arr1 = [3,7,34,90,12];
+let arr2 = [true, "green", "where",12,56];
+const result=(arr1[arr1.length-1]);
+const resultOne=(arr2[arr2.length-1]);
+console.log({result});
+console.log({resultOne});
+
+let myPets = ["Cow", "Bird", "Snake", "Dog"];
+let res=myPets.join(" ");
+console.log({res});
+
+
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+console.log(arr3.sort());
+
+
+let arr = ["boy", "man", "girl", "school", "girl", "woman"];
+arr = arr.filter((item, index) => arr.indexOf(item) === index);
+console.log(arr)
+
+let arr5 = ["the","way","x",4]
+let findingFood = arr5.find(arr5 => arr5 == "Food");
+if (findingFood == "Food"){
+   console.log(findingFood)
+}
+   else{
+console.log("the search was not found")
+   }
+
+let word = "renniw"
+const ans = word.split('').reverse().join('');
+console.log(ans);
+
+let fruits=["apple","mango","watermelon","cucumber","Orange","Banana","Kiwi","currant","Strawberry","Papaya"]
+let fruit="Tomato";
+addFruit=fruits.slice(0,5)+fruit+","+fruits.slice(5);
+console.log(addFruit);
